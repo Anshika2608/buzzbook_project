54467b86964037944e51d555df1229ad5e2df650
Kritika Yadav <yadavkritika745@gmail.com>
2025-09-11 23:34:32 +0530

    now showing

--- buzzbook/app/home/page.tsx
+++ buzzbook/app/home/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import React from 'react';
-
-// Assuming your files are at these paths, please adjust if they are different
 import { LocationProvider, useLocation } from '@/app/context/LocationContext';
-import HeroCarousel from '@/components/home/Herocarousel'; // This imports YOUR carousel component
+import HeroCarousel from '@/components/home/Herocarousel'; 
+import NowShowing from '@/components/home/NowShowing';
 
 
-// --- Main Page Layout ---
-// This is the core content of your homepage. It uses the data from the context.
 const HomePageContent = () => {
     const { comingSoonMovies, isLoadingComingSoon } = useLocation();
     console.log("Carousel movies in homepage:", comingSoonMovies);
@@ -16,8 +13,9 @@ const HomePageContent = () => {
 
     return (
         <div className="bg-gray-900 min-h-screen font-sans">
-            {/* Renders YOUR HeroCarousel component, passing the required data. */}
             <HeroCarousel movies={comingSoonMovies} isLoading={isLoadingComingSoon} />
+            <NowShowing />
+
             
         </div>
     );
